3f0be9023be5ee2caf3b20ab6afaafeca2259b65
Elton Manoku <elton@meta-invent.com>
2020-10-13 11:08:28 +0200

    De ReSerialize.sh is aangepast om door de files heen te kunnen navigeren. Ook de javaapp is met de juiste parameters geroepen. pre-commit is aangepast zodat de exit codes 0 of 1 zijn SUCCESS codes. Het was namelijk alleen code 0 als SUCCESS gezien. Verder de reserialize javaapp is in pre-commit ook met de juiste parameters geroepen. Deze pre-commit moet ook in de .git/hooks gekopieerd worden in github.

--- ReSerialize.sh
+++ ReSerialize.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-
-for f in ontology/*.ttl; do
-    echo "ReSerializing file: $f"
-    tools/ReSerialize/bin/ReSerialize -f $f -r
-    if [ $? -gt 0 ]; then
-        echo "> Modified file: $f"
-    fi
+for f in `find ontology -type f -name "*.ttl"`; do 
+  echo "ReSerializing file: $f"
+  tools/ReSerialize/bin/ReSerialize -f $f -r
 done
